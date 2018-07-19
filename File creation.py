21b6a88f57f9478e4722175385670577ebdf48cd
RevengeofTheBenge <38697160+RevengeofTheBenge@users.noreply.github.com>
2018-07-19 20:54:13 +0100

    Add files via upload

--- File creation.py
+++ File creation.py
@@ -6,6 +6,8 @@ iflogin = False
 
 filename = ""
 
+usr = ""
+
 def md5hash(s):
      hash_object = hashlib.md5(s.encode())
      return hash_object.hexdigest()
@@ -15,29 +17,31 @@ def spaces():
           print(" ")
 
 def percentage(s):
-     for i in range(1, 99):
+     for i in range(1, 101):
           print(s +"... " + str(i) + "%")
 
 def login():
+     global usr
      global iflogin
      global filename
-     spaces()
+     print("")
      usr = input("Please enter a username: ")
      pas = input("Please enter a password: ")
      filename = usr + pas  
      filename = md5hash(filename) + ".txt"
-     percenntage("Login in")
+     percentage("Logging in")
+     print("Logged in.")
      iflogin = True
      
 def accountcreator():
+     global iflogin
+     global filename
      spaces()
      input("You are about to create an account. Press <ENTER> to continue.")
-     print("")
-     usr = input("Please enter a username: ")
-     pas = input("Please enter a password: ")
-     filename = usr + pas  
-     filename = md5hash(filename) + ".txt"
-     print("")
+     if iflogin == False:
+          login()
+          time.sleep(1.5)
+          spaces()
      balance = input("Would you like to deposit money into your account?(y/n): ")
      if balance == 'y':
           print("")
@@ -60,19 +64,20 @@ def accountcreator():
 
 def accountopener():
      spaces()
+     global filename
+     global iflogin
      input("Welcome back, friend... Press <ENTER> to continue.")
+     if iflogin == False:
+          login()
+          print("")
+          input("Press <ENTER> to continue.")
      print("")
-     usr1 = input("Please enter your username: ")
-     pas1 = input("Please enter your password: ")
-     print("")
-     opener = md5hash(usr1 + pas1) + ".txt"
-     f=open(opener,"r")
+     f=open(filename,"r")
      percentage('Opening')
      print("Account opened.")
      time.sleep(1)
      spaces()
-     print("Current bank balance: ")
-     print('       £' + f.read())
+     print("Current bank balance: £" + f.read())
      print("")
      f.close()
      input('Press <ENTER> to exit.')
@@ -80,26 +85,35 @@ def accountopener():
 
 def accountcloser():
      spaces()
+     global iflogin
+     global filename
+     global usr
      input("I see you want to delete your account. Very well. Press <ENTER> to continue.")
+     if iflogin == False:
+          login()
+          print("")
+          input("Press <ENTER> to continue.")
      print("")
-     usr1 = input("Please enter your username: ")
-     pas1 = input("Please enter your password: ")
-     print("")
-     opener = md5hash(usr1 + pas1) + ".txt"
-     os.remove(opener)
+     os.remove(filename)
      percentage('Deleting')
      print("Account deleted.")
+     filename = ""
+     usr = ""
+     iflogin = False
      time.sleep(1)
      spaces()
 
 def depositer():
+     global iflogin
+     global filename
      spaces()
      input('I see you want to deposit some money. Press <ENTER> to continue. ')
+     if iflogin == False:
+          login()
+          print("")
+          input("Press <ENTER> to continue.")
+          spaces()
      print("")
-     usr = input("Please enter your username: ")
-     pas = input("Please enter your password: ")
-     print("")
-     filename = md5hash(usr + pas) + '.txt'
      f = open(filename, 'r+')
      balance = int(f.read())
      f.truncate(0)
@@ -114,13 +128,17 @@ def depositer():
      spaces()
 
 def remover():
+     global iflogin
+     global filename
      spaces()
      input('I see you want to withdraw some money. Press <ENTER> to continue. ')
+     if iflogin == False:
+          login()
+          print("")
+          time.sleep(1)
+          input("Press <ENTER> to continue.")
+          spaces()
      print("")
-     usr = input("Please enter your username: ")
-     pas = input("Please enter your password: ")
-     print("")
-     filename = md5hash(usr + pas) + '.txt'
      f = open(filename, 'r+')
      balance = int(f.read())
      f.truncate(0)
@@ -135,38 +153,51 @@ def remover():
      spaces()
 
 def select():
+     global usr
+     global iflogin
      print("Choose 1 of the following options:")
      print("")
-     if login == True:
-          print("Logged in.")
+     if iflogin == False:
+          print("Logged out.")
+     elif iflogin == True:
+          print("Logged in to account: " + usr)
      print("")
      print("      [1] - Open an account.")
-     print("      [2] - Check your bank balance.")
-     print("      [3] - Close an account.")
+     if iflogin == False:
+          print("      [2] - Login to your account.")
+     elif iflogin == True:
+          print("      [2] - Logout of your account.")
+     print("      [3] - Check your bank balance.")
      print("      [4] - Make a deposit.")
      print("      [5] - Make a withdrawal.")
-     if login == False:
-          print("      [6] - Login to your account.")
-          login()
-     elif login == True:
-          print("      [6] - Logout of your account.")
-          global iflogin
-          iflogin = False
+     print("      [6] - Close an account.")
      print("      [7] - Exit the bank.")
      print("")
      choice = input("Choice: ")
      if choice == "1":
           accountcreator()
-     elif choice == "2":
-          accountopener()
      elif choice == "3":
+          accountopener()
+     elif choice == "6":
           accountcloser()
      elif choice == '4':
           depositer()
      elif choice == '5':
           remover()
-     elif choice == "6":
+     elif choice == '2' and iflogin == False:
+          login()
+          time.sleep(1.5)
+          spaces()
+     elif choice == '2' and iflogin == True:
+          percentage("Logging out")
+          print("Logged out.")
+          time.sleep(1.5)
+          iflogin = False
+          spaces()
+     elif choice == "7":
           return 1
+     else:
+          spaces()
 
      
 def start():
